1749499fd01e95ae11fce7e9f2bbc928ef360465
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-04-01 13:54:12 +0530

    Add assets, FeatureCard component, and router
    Install react-router-dom and update package.json/package-lock, add new SVG/PNG assets (body-part-muscle, wellness, workout-stretching, workout-stretching1), remove unused hero/react/vite images, introduce FeatureCard atom component, and update Home.jsx to integrate these UI changes. package-lock was updated to reflect the new dependency installation.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import FeatureCard from '../components/Atoms/FeatureCard/FeatureCard'
+import bodyBuilding from "../assets/body-part-muscle.svg"
+import personalTraining from "../assets/wellness.svg"
+import cardio from "../assets/workout-stretching.svg"
+import yoga from "../assets/workout-stretching1.svg"
+
 
 const Home = () => {
   return (
-    <section className='bg-amber-300 h-full w-full flex items-center'>
-        <div>
+    <section className='bg-amber-300 h-auto w-full flex items-center justify-center flex-col'>
+        <div className='bg-green-500 h-screen w-full'>
             <h1>Hero section</h1>
         </div>
+        <div className='h-[50vh] w-full bg-fuchsia-400 flex flex-col justify-center items-center'>
+            <div className='flex flex-col items-center justify-center border border-amber-400 h-[30%] w-full'>
+                <h2 className='text-4xl mb-2 font-extrabold'>Transform Your Body</h2>
+                <p className='text-l text-black opacity-70'>Choose the right program based on your goals — strength, weight loss, or overall fitness.</p>
+            </div>
+            <div className='flex flex-row items-center justify-center border border-amber-400 h-[70%] w-full gap-7'>
+            <FeatureCard img={bodyBuilding} heading={"Weight Training"}/>
+            <FeatureCard img={personalTraining} heading={"Personal Training"}/>
+            <FeatureCard img={cardio} heading={"Cardio / HIIT"}/>
+            <FeatureCard img={yoga} heading={"Yoga / Zumba"}/>
+            </div>
+        </div>
+        <div className='h-[90vh] w-full border border-amber-400'></div>
     </section>
   )
 }
