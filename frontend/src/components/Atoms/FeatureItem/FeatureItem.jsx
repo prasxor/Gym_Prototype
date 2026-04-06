48addf8f00a9c1891b1ad28afba9aeab416b4d8e
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-04-06 22:07:28 +0530

    feat : updated content of the pricing and color factor

--- frontend/src/components/Atoms/FeatureItem/FeatureItem.jsx
+++ frontend/src/components/Atoms/FeatureItem/FeatureItem.jsx
@@ -2,7 +2,7 @@
 const FeatureItem = ({ children }) => {
   return (
     <li className="flex items-center justify-left space-x-2">
-      <span className="flex items-center justify-center w-5 h-5 bg-teal-500 text-white rounded-full">
+      <span className="flex items-center justify-center w-5 h-5 bg-black text-white rounded-full">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={14} height={14}>
           <path fill="none" d="M0 0h24v24H0z" />
           <path
